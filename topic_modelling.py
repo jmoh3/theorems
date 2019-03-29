7ecafdad21ba4ee4e8f8e8b1ca123b0d29976b92
jmoh3 <jmoh3@illinois.edu>
2019-03-29 12:20:52 -0500

    Better variable naming

--- topic_modelling.py
+++ topic_modelling.py
@@ -20,20 +20,20 @@ tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features,
 tf = tf_vectorizer.fit_transform(documents)
 tf_feature_names = tf_vectorizer.get_feature_names()
 
-no_topics = 20
+num_topics = 20
 
 # Run NMF
-nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
+nmf = NMF(n_components=num_topics, random_state=1, alpha=.1, l1_ratio=.5, init='nndsvd').fit(tfidf)
 
 # Run LDA
-lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
+lda = LatentDirichletAllocation(n_topics=num_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
 
-def display_topics(model, feature_names, no_top_words):
+def display_topics(model, feature_names, num_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print("Topic %d:" % (topic_idx))
         print(" ".join([feature_names[i]
-                        for i in topic.argsort()[:-no_top_words - 1:-1]]))
+                        for i in topic.argsort()[:-num_top_words - 1:-1]]))
 
-no_top_words = 10
-display_topics(nmf, tfidf_feature_names, no_top_words)
-display_topics(lda, tf_feature_names, no_top_words)
+num_top_words = 10
+display_topics(nmf, tfidf_feature_names, num_top_words)
+display_topics(lda, tf_feature_names, num_top_words)
